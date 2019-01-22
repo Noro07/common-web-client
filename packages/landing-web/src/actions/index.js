21bfc01aee2ecdb2474d566811a5970134d8ad93
Noro <uniform328@gmail.com>
2019-01-22 15:08:53 +0800

    use prettier

--- packages/landing-web/src/actions/index.js
+++ packages/landing-web/src/actions/index.js
@@ -1,15 +1,17 @@
 import { fetchUtils } from 'utils-web';
 import * as ACTION_TYPE from '../constants/ActionType';
 
-export const loadUser = user => ({
+export const loadUser = (user) => ({
   type: ACTION_TYPE.LOAD_USER,
   data: user
 });
 
 export const fetchUser = () => (dispatch) => {
   const requestURL = '/api/user';
-  fetchUtils.fetchJSON(requestURL).then((json) => {
-    dispatch(loadUser(json.user));
-  }, () => {
-  });
+  fetchUtils.fetchJSON(requestURL).then(
+    (json) => {
+      dispatch(loadUser(json.user));
+    },
+    () => {}
+  );
 };
